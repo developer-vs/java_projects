84d120d7797bf6614d99e0fca128cb6ccd7e94d2
Vladimir Semenovich <vladimirgdsr@gmail.com>
2019-12-24 14:16:52 -0800

    implementation of HW_1

---
+++ lesson_1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte osType = 64;
+		short diskSize = 80;
+		int cpu = 2450;
+		long serial = 2356798789715178L;
+		float memorySize = 7.6F;
+		double cpuMHz = 798.250D;
+		char virtualization = 'X';
+		boolean isWindows = false;
+
+		System.out.println(osType);
+		System.out.println(diskSize);
+		System.out.println(cpu);
+		System.out.println(serial);
+		System.out.println(memorySize);
+		System.out.println(cpuMHz);
+		System.out.println(virtualization);
+		System.out.println(isWindows);
+	}
+}
