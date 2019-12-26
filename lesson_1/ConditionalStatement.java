2e700d2bc7660c9cefbff7b3b7a8a7361019c5d8
Vladimir Semenovich <vladimirgdsr@gmail.com>
2019-12-26 10:08:36 -0800

    implemented the second method of a boolean expression

--- lesson_1/ConditionalStatement.java
+++ lesson_1/ConditionalStatement.java
@@ -1,29 +1,27 @@
 public class ConditionalStatement {
-    public static void main(String[] args) {
-    
-        int age = 18;
-        char sex = 'F';
-        float height = 1.70f;
-        char firstLetterOfTheName = 'T';
-        
+    public static void main(String[] args) {    
+        int age = 18;         
         if(age > 20) {
             System.out.println("Your age is over 20.");
         }
 
-        if(sex == 'M') {
+        boolean isMale = true;
+        if(isMale) {
             System.out.println("You are a man.");
         }
 
-        if(sex != 'M') {
+        if(!isMale) {
             System.out.println("You are not a man.");
         }
 
+        float height = 1.70f;
         if(height < 1.80) {
             System.out.println("Your height is less than 1.80.");
         } else {
             System.out.println("Your height is more than 1.80.");
         }
 
+        char firstLetterOfTheName = 'T';
         if(firstLetterOfTheName == 'M') {
             System.out.println("Your first letter of the name is M.");
         } else if(firstLetterOfTheName == 'I') {
