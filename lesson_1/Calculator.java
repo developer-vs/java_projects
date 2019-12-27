5f5d78e0c5ac43b52083f3b7cca25aedc67d7954
Vladimir Semenovich <vladimirgdsr@gmail.com>
2019-12-26 20:42:24 -0800

    implementation of HW_1, task 4

---
+++ lesson_1/Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int num1 = 2;
+		int num2 = 3;
+		char symbol = '^';
+
+		if(symbol == '+') {
+			System.out.println(num1 + num2);
+		} else if(symbol == '-') {
+			System.out.println(num1 - num2);
+		} else if(symbol == '*') {
+			System.out.println(num1 * num2);
+		} else if(symbol == '/') {
+			System.out.println(num1 / num2);
+		} else if(symbol == '^') {
+			int result = 1;
+			if(num2 != 0) {
+			    result = num1;
+			    for(int i = 1; i < num2; i++) {
+				    result = result * num1;
+			    }			
+			}			
+			System.out.println(result);
+		} else if(symbol == '%') {
+			System.out.println(num1 % num2);
+		}
+	}
+}
