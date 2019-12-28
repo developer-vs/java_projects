28af103d2dd474935d51637d4e9f12d462e726fd
Vladimir Semenovich <vladimirgdsr@gmail.com>
2019-12-27 20:25:15 -0800

    implementation of HW_1, task 6

---
+++ lesson_1/Unicode.java
@@ -0,0 +1,16 @@
+public class Unicode {
+  public static void main(String[] args) {
+    for (char i = 9398; i < 10178; i++) {
+        System.out.print(i + " ");
+    } 
+    
+    System.out.println();
+    System.out.println();
+    
+    for (char i = 33; i < 126; i++) {
+        System.out.print(i + " ");
+    } 
+    
+    System.out.println();
+  }
+}
