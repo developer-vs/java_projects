834ca9c17db851863d63405dddebf621e7daf8d5
Vladimir Semenovich <vladimirgdsr@gmail.com>
2019-12-29 10:12:06 -0800

    code refactoring of HW_1, task 5 (GuessNumber)

--- lesson_1/GuessNumber.java
+++ lesson_1/GuessNumber.java
@@ -1,14 +1,20 @@
 public class GuessNumber {
-    public static void main(String[] args) {
-        int guessNumber = 30;
-        int myNumber = 18;
-        
-        if(myNumber > guessNumber) {
-            System.out.println("Введенное вами число больше того, что загадал компьютер");
-        } else if(myNumber < guessNumber) {
-            System.out.println("Введенное вами число меньше того, что загадал компьютер");
-        } else if(myNumber == guessNumber) {
-            System.out.println("Вы угадали!");
-        }
-    }
+	public static void main(String[] args) {
+		int guessNumber = 30;
+		int myNumber = 28;
+		
+		while(myNumber != guessNumber) {		
+			if(myNumber > guessNumber) {
+				System.out.println("Введенное вами число больше того, что загадал компьютер");
+				myNumber--;
+			}
+		
+			if(myNumber < guessNumber) {
+				System.out.println("Введенное вами число меньше того, что загадал компьютер");
+				myNumber++;
+			}
+		}
+		
+		System.out.println("Вы угадали!");
+	}
 }
