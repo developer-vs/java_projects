7f0714f9dfb5514a5b2a206489c6484155b16a33
Vladimir Semenovich <vladimirgdsr@gmail.com>
2020-01-04 14:18:13 -0800

    implementation of HW_2, task_1 (Person)

---
+++ lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+	char sex = 'M';
+	String name = "John";
+	int height = 185;
+	float weight = 78.3f;
+	int age = 30;
+	
+	public void walk() {
+		System.out.println("I'm walking");
+	}
+	
+	public void seat() {
+		System.out.println("I'm seating");
+	}
+	
+	public void run() {
+		System.out.println("I'm running");
+	}
+	
+	public void saySomething() {
+		System.out.println("Hi!");
+	}
+	
+	public void learnJava() {
+		System.out.println("I'm learning Java!");
+	}
+}
