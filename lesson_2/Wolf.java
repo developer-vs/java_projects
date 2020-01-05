b864041823c321fd714303f08500e7445efa9f29
Vladimir Semenovich <vladimirgdsr@gmail.com>
2020-01-04 16:24:03 -0800

    implementation of HW_2, task_2

---
+++ lesson_2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+	char sex;
+	String nickName;
+	float weight;
+	int age;
+	String color;
+	
+	public void go() {
+		System.out.println("Wolf will go to the forest"); 
+	}
+	
+	public void seat() {
+		System.out.println("Wolf is seating");	
+	}
+	
+	public void run() {
+		System.out.println("Wolf is running");	
+	}
+	
+	public void howl() {
+		System.out.println("Wolf is howling");	
+	}
+	
+	public void hunt() {
+		System.out.println("Wolf is hunting");
+	}
+}
---
+++ lesson_2/WolfTest.java
@@ -0,0 +1,24 @@
+public class WolfTest {
+
+  public static void main(String[] args) {
+    Wolf wolf = new Wolf();
+    
+    wolf.sex = 'M';
+    wolf.nickName = "Fluffy";
+    wolf.weight =  4.3f;
+    wolf.age = 5;
+    wolf.color = "grey";
+    
+    System.out.println("sex: " + wolf.sex);
+    System.out.println("nickName: " + wolf.nickName);
+    System.out.println("weight: " + wolf.weight);
+    System.out.println("age: " + wolf.age);
+    System.out.println("color: " + wolf.color);
+    
+    wolf.go();
+    wolf.seat();
+    wolf.run();
+    wolf.howl();
+    wolf.hunt();
+  }
+}
