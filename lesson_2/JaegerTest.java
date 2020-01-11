803ee583cb70cf7d1e9cb818a82b71b4c073207c
Vladimir Semenovich <vladimirgdsr@gmail.com>
2020-01-11 08:15:15 -0800

    code refactoring of HW_2, task_4

--- lesson_2/JaegerTest.java
+++ lesson_2/JaegerTest.java
@@ -2,24 +2,20 @@ public class JaegerTest {
     
     public static void main(String[] args) {
         Jaeger jaegerOne = new Jaeger();
-        Jaeger jaegerTwo = new Jaeger();
-
         jaegerOne.setModelName("Striker Eureka");
         jaegerOne.setMark("Mark-6");
         jaegerOne.setOrigin("Australia");
         jaegerOne.setSpeed(10);
+        System.out.println(jaegerOne.getSpeed());
+        System.out.println(jaegerOne.checkStatus());
 
+        Jaeger jaegerTwo = new Jaeger();
         jaegerTwo.setModelName("Cherno Alpha");
         jaegerTwo.setMark("Mark-1");
         jaegerTwo.setOrigin("Russia");
-        jaegerTwo.setSpeed(4);
-
-        System.out.println(jaegerOne.getSpeed());
+        jaegerTwo.setSpeed(4);        
         System.out.println(jaegerTwo.getSpeed());
-
-        System.out.println(jaegerOne.checkStatus());
         System.out.println(jaegerTwo.checkStatus());
-
         jaegerTwo.setSpeed(2);
         System.out.println(jaegerTwo.getSpeed());
     }
