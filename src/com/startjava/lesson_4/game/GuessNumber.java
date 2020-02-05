e0b08898a29faad7fe3ddf97cdc35b7f973f4252
Vladimir Semenovich <vladimirgdsr@gmail.com>
2020-02-05 15:22:58 -0800

    code refactoring of HW_4, task_2

--- src/com/startjava/lesson_4/game/GuessNumber.java
+++ src/com/startjava/lesson_4/game/GuessNumber.java
@@ -8,7 +8,7 @@ public class GuessNumber {
     private Player playerOne;
     private Player playerTwo;
     private int guessNumber;
-    private int counter = 0;
+    private int counter;
     private Scanner scanner = new Scanner(System.in);
 
     public GuessNumber(Player playerOne, Player playerTwo) {
@@ -19,44 +19,38 @@ public class GuessNumber {
     public void startGame() {
         guessNumber = (int) (Math.random() * 101);
         System.out.println("Компьютер загадал число: " + guessNumber);
-        
-        while(counter < 10) {
-            if(makeMove(playerOne)) {
-                clearAnArrayForBothPlayers();
-                counter = 0;
+
+        System.out.println(Arrays.toString(Arrays.copyOf(playerOne.getEnteredNumbers(), counter)));
+        clearAnArray(playerOne);
+        clearAnArray(playerTwo);
+        counter = 0;
+
+        while (counter < 10) {
+            if (makeMove(playerOne)) {
+                counter++;
                 break;
             }
-            if(makeMove(playerTwo)) {
-                clearAnArrayForBothPlayers();
-                counter = 0;
+            if (makeMove(playerTwo)) {
+                counter++;
                 break;
             }
             counter++;
         }
 
-        if(counter == 10) {
+        if (counter == 10) {
             System.out.println("У " + playerOne.getName() + " закончились попытки");
-            System.out.println(Arrays.toString(playerOne.getEnteredNumbers()));
-            clearAnArrayForBothPlayers();
+            System.out.println(Arrays.toString(Arrays.copyOf(playerOne.getEnteredNumbers(), counter)));
 
             System.out.println("У " + playerTwo.getName() + " закончились попытки");
-            System.out.println(Arrays.toString(playerTwo.getEnteredNumbers()));
-            clearAnArrayForBothPlayers();
-            counter = 0;
+            System.out.println(Arrays.toString(Arrays.copyOf(playerTwo.getEnteredNumbers(), counter)));
         }
     }
 
     private void clearAnArray(Player player) {
-        for(int i = 0; i < counter + 1; i++) {
-            player.setEnteredNumbers(i, 9);
-        }
-    }
-
-    private void clearAnArrayForBothPlayers() {
-        clearAnArray(playerOne);
-        System.out.println(Arrays.toString(playerOne.getEnteredNumbers()));
-        clearAnArray(playerTwo);
-        System.out.println(Arrays.toString(playerTwo.getEnteredNumbers()));
+        //for (int i = 0; i < counter; i++) {
+            // player.setEnteredNumbers(i, 9);
+            Arrays.fill(player.getEnteredNumbers(), 0);
+        //}
     }
 
     private boolean makeMove(Player player) {
@@ -69,18 +63,19 @@ public class GuessNumber {
         System.out.print(player.getName() + ", введите ваше число: ");
         player.setNumber(scanner.nextInt());
     }
-    
+
     private void addNumberToArray(Player player) {
         player.setEnteredNumbers(counter, player.getNumber());
     }
 
     private boolean compareNumbers(Player player) {
-        if(player.getNumber() > guessNumber) {
+        if (player.getNumber() > guessNumber) {
             System.out.println(player.getName() + ", введенное вами число больше того, что загадал компьютер");
-        } else if(player.getNumber() < guessNumber) {
+        } else if (player.getNumber() < guessNumber) {
             System.out.println(player.getName() + ", введенное вами число меньше того, что загадал компьютер");
-        } else if(player.getNumber() == guessNumber) {System.out.println("Игрок " + player.getName() +
-                " угадал число " + player.getNumber() + " с " + (counter + 1) + " попытки");
+        } else if (player.getNumber() == guessNumber) {
+            System.out.println("Игрок " + player.getName() +
+                    " угадал число " + player.getNumber() + " с " + (counter + 1) + " попытки");
             return true;
         }
         return false;
