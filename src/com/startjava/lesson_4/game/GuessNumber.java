de728d0fbf59717967b284a32d2e716929bcc170
Vladimir Semenovich <vladimirgdsr@gmail.com>
2020-02-05 12:01:47 -0800

    code refactoring of HW_4, task_2

--- src/com/startjava/lesson_4/game/GuessNumber.java
+++ src/com/startjava/lesson_4/game/GuessNumber.java
@@ -22,14 +22,40 @@ public class GuessNumber {
         
         while(counter < 10) {
             if(makeMove(playerOne)) {
+                clearAnArrayForBothPlayers();
+                counter = 0;
                 break;
             }
             if(makeMove(playerTwo)) {
+                clearAnArrayForBothPlayers();
+                counter = 0;
                 break;
             }
             counter++;
         }
+
+        if(counter == 10) {
+            System.out.println("У " + playerOne.getName() + " закончились попытки");
+            System.out.println(Arrays.toString(playerOne.getEnteredNumbers()));
+            clearAnArrayForBothPlayers();
+
+            System.out.println("У " + playerTwo.getName() + " закончились попытки");
+            System.out.println(Arrays.toString(playerTwo.getEnteredNumbers()));
+            clearAnArrayForBothPlayers();
+            counter = 0;
+        }
+    }
+
+    private void clearAnArray(Player player) {
+        for(int i = 0; i < counter + 1; i++) {
+            player.setEnteredNumbers(i, 9);
+        }
+    }
+
+    private void clearAnArrayForBothPlayers() {
+        clearAnArray(playerOne);
         System.out.println(Arrays.toString(playerOne.getEnteredNumbers()));
+        clearAnArray(playerTwo);
         System.out.println(Arrays.toString(playerTwo.getEnteredNumbers()));
     }
 
@@ -54,7 +80,7 @@ public class GuessNumber {
         } else if(player.getNumber() < guessNumber) {
             System.out.println(player.getName() + ", введенное вами число меньше того, что загадал компьютер");
         } else if(player.getNumber() == guessNumber) {System.out.println("Игрок " + player.getName() +
-                " угадал число " + player.getNumber() + " с " + counter + " попытки");
+                " угадал число " + player.getNumber() + " с " + (counter + 1) + " попытки");
             return true;
         }
         return false;
