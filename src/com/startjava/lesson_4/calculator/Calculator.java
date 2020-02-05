f2e9562de18a4a756ad8e1c9c47a5bf99f77d7fb
Vladimir Semenovich <vladimirgdsr@gmail.com>
2020-02-05 09:23:19 -0800

    code refactoring of HW_4, task_1

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -5,36 +5,38 @@ public class Calculator {
     private int num1;
     private int num2;
     private char mathOperator;
+    private int value;
 
-    public void setExpression(String expression) {
+    public Calculator(String expression) {
         String[] arr = expression.split(" ");
         num1 = Integer.parseInt(arr[0]);
         num2 = Integer.parseInt(arr[2]);
         mathOperator = arr[1].charAt(0);
     }
 
-    public void calculate() {
+    public int calculate() {
         switch(mathOperator) {
             case '+':
-                System.out.println(Math.addExact(num1, num2));
+                value = Math.addExact(num1, num2);
                 break;
             case '-':
-                System.out.println(Math.subtractExact(num1, num2));
+                value = Math.subtractExact(num1, num2);
                 break;
             case '*':
-                System.out.println(Math.multiplyExact(num1, num2));
+                value = Math.multiplyExact(num1, num2);
                 break;
             case '/':
-                System.out.println(Math.floorDiv(num1, num2));
+                value = Math.floorDiv(num1, num2);
                 break;
             case '^':
-                System.out.println(Math.pow(num1, num2));
+                value = (int)Math.pow(num1, num2);
                 break;
             case'%':
-                System.out.println(Math.floorMod(num1, num2));
+                value = Math.floorMod(num1, num2);
                 break;
             default:
                 System.out.println("Вы использовали неправильный математический оператор");
         }
+        return value;
     }
 }
--- src/com/startjava/lesson_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_4/calculator/CalculatorTest.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
         Scanner scan = new Scanner(System.in);
         String answer = "";
 
         while(!answer.equals("нет")) {
             System.out.print("Введите математическое выражение: ");
-            calc.setExpression(scan.nextLine());
-
-            calc.calculate();
+            Calculator calc = new Calculator(scan.nextLine());
+            System.out.println(calc.calculate());
 
             do {
                 System.out.print("Хотите продолжить? [да/нет]: ");
